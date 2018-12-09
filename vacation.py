a24f0b1da471711d92ce35f2fab26617e44a5bc4
Blanca <38861552+brcmd@users.noreply.github.com>
2018-12-09 18:12:49 +0100

    Add files via upload

---
+++ vacation.py
@@ -0,0 +1,21 @@
+def hotel_cost(nights):
+  return  140 * nights
+def plane_ride_cost(city):
+  if city == "Charlotte":
+      return 183
+  if city == "Tampa":
+      return 220
+  if city == "Pittsburgh":
+      return 222
+  if city == "Los Angeles":
+      return 475
+def rental_car_cost(days):
+	total = 40 * days
+	if days >= 7:
+		total -= 50
+	elif days >= 3:
+		total -= 20
+	return total
+def trip_cost(city, days, spending_money):
+  return rental_car_cost(days) + hotel_cost(days -1) + plane_ride_cost(city) + spending_money
+print(trip_cost("Los Angeles", 5, 600))
